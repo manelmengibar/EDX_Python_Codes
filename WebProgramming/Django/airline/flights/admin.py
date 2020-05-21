2440b81605728e56f87e1c3e590ecfc271bd5dfa
Manel Mengíbar <manelmengibar@gmail.com>
2020-05-21 16:07:16 +0200

    admin file in airline Django project edited

--- WebProgramming/Django/airline/flights/admin.py
+++ WebProgramming/Django/airline/flights/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 
 from .models import Airport, Flight, Passenger
 
+class PassengerInline(admin.StackedInline):
+    model = Passenger.flights.through
+    extra=1
+
+class FlightAdmin(admin.ModelAdmin):
+    inlines = [PassengerInline]
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
 # Register your models here.
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
